26edb253369cfcd80d52c2ca3d5d55454cb8e8cd
Zainab Hussain <164765199+zainab-hussain-1@users.noreply.github.com>
2024-06-24 14:45:16 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,27 @@
+const inputbox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputbox.value===''){
+        alert("You must write something");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML = inputbox .value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+
+    }
+    inputbox.value="";
+}
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("chceked");
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
+
